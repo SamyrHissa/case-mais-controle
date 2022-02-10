6f4c79da4c3c203c0042d29bafb5c74ad123b20a
PCSAMYRCASA\samyr <samyr@schissa.com.br>
2022-02-10 20:25:52 -0300

    Inicio dos testes unitários

--- back/test/Acoes.Business.test.ts
+++
@@ -1,7 +0,0 @@
-import AcoesBusiness from "../src/business/acoes/Acoes.Business";
-import { AlertAcoesMock } from "./mocks/alertAcoesMock";
-
-const AcoesBusinessMock = new AcoesBusiness (
-    new AlertAcoesMock()
-)
-
